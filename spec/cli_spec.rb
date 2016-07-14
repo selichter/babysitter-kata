87c0ab6cc305276785b45a80a64e5d95b710ab5c
selichter <lichter.sarahe@gmail.com>
2016-07-13 21:45:16 -0500

    refactored cli_spec

--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -1,27 +1,26 @@
 require 'spec_helper'
 
 describe BabysitterKata::CLI do
-
+  let(:cli){BabysitterKata::CLI.new}
   it 'is defined' do
     expect(BabysitterKata::CLI).to be_a(Class)
   end
 
 
   describe '#greet_user' do
-    it 'Welcomes the user upon call' do
-      cli = BabysitterKata::CLI.new
 
+    it 'Welcomes the user upon call' do
       expect{cli.call}.to output("Welcome\n").to_stdout
     end
 
   end
 
   describe '#get_start_time' do
+
     it 'asks the user for when their job started' do
-      cli = BabysitterKata::CLI.new
       expect{cli.get_start_time}.to output("What time did your babysitting job start?\n").to_stdout
     end
-  end
 
+  end
 
 end
