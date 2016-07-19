85b60f50b1139b0fbdcd1f811199690a04c59bab
selichter <lichter.sarahe@gmail.com>
2016-07-18 19:00:50 -0500

    moved #parse time and #get_hour methods and tests to the hour module -- Haven't removed them from the Job class yet

--- spec/hour_spec.rb
+++ spec/hour_spec.rb
@@ -5,5 +5,24 @@ describe 'Hour module' do
     expect(test_class.test_method).to eq("I'm testing the Hour module")
   end
 
+  describe '#parse_time' do
+    it 'parses times passed to the method as a string' do
+      str = "5pm"
+      expect(test_class.parse_time(str)).to be_instance_of(Time)
+    end
+  end
+
+  describe '#get_hour' do
+    it 'returns the current hour when time passed is on the hour' do
+      leave_time = "1am"
+      expect(test_class.get_hour(leave_time)).to eq(1)
+    end
+
+    it 'returns the next hour if time passed is not on the hour' do
+        leave_time = "1:01am"
+        expect(test_class.get_hour(leave_time)).to eq(2)
+    end
+  end
+
 
 end
