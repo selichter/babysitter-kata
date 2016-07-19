e730a00488804b3f9544ce0ec67048772c724d29
selichter <lichter.sarahe@gmail.com>
2016-07-18 20:40:50 -0500

    added test/method to validate a start time

--- lib/babysitter_kata/validator.rb
+++ lib/babysitter_kata/validator.rb
@@ -1,8 +1,17 @@
+require_relative './hour.rb'
 
 module Validator
+  include Hour
+
+  AVAILABLE_START_TIME = 17
+  LEAVE_BY = 4
 
   def test_method
     "This is a test method"
+  end
+
+  def valid_start?(time)
+    parse_time(time).hour >= AVAILABLE_START_TIME ? true: false
   end
 
 end
--- spec/validator_spec.rb
+++ spec/validator_spec.rb
@@ -5,4 +5,17 @@ describe 'Validator module' do
     expect(test_class.test_method).to eq("This is a test method")
   end
 
+  describe '#valid_start? ' do
+    it 'returns true is start time is valid' do
+      time = '5pm'
+      expect(test_class.valid_start?(time)).to be(true)
+    end
+
+    it 'raises an error if the start time is not valid' do
+      time = '4pm'
+      expect(test_class.valid_start?(time)).to be(false)
+
+    end
+  end
+
 end
